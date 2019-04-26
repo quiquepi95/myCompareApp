6db1a171cddf7ea358c3a0ffb99d301eee8be39a
Enrique Pintado <epintadom@eservicios.indra.es>
2019-04-26 14:44:16 +0200

    probaturas con scroll

--- src/components/compareItem/compareItem.ts
+++ src/components/compareItem/compareItem.ts
@@ -7,6 +7,7 @@ import { ListaService } from "../../services/lista.service";
 })
 export class CompareItemComponent implements OnInit {
 
+
     @Input() catSeleccionada: string;
     itemSeleccionadoIzq: any;
     itemSeleccionadoDrcha: any;
@@ -14,7 +15,8 @@ export class CompareItemComponent implements OnInit {
     listaDrcha: any[] = [];
     subcat: string;
 
-    constructor(public listaService: ListaService){ }
+    constructor(public listaService: ListaService){
+        }
 
     ngOnInit(){ }
 
--- src/pages/compare/compare.ts
+++ src/pages/compare/compare.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ListaService } from '../../services/lista.service';
+
+enum StickyState {
+  fixed =  "fixed",
+  noFixed =  "no-fixed"
+}
 
 @Component({
   selector: 'page-compare',
@@ -8,16 +13,47 @@ import { ListaService } from '../../services/lista.service';
 })
 export class ComparePage implements OnInit {
 
+  private fixedState = StickyState.noFixed;
+  private initialOffsetFromTop =  0;
+  private fixedViewportOffset = 0;
+
   catSeleccionada: string;
   datosCategoria: any = [];
 
   constructor(public navCtrl: NavController,
-              public listaService: ListaService ) {
+              public listaService: ListaService,
+              private element: ElementRef,
+              private renderer:Renderer2 ) {
 
   }
   
   ngOnInit(){
     this.datosCategoria = ['vehiculos','electrodomesticos','electronica'];
   }
+
+  // logScrolling(event: Event) {
+  //   console.log('entro en scroll');
+  //   console.log(event);
+    
+  //   let currentScroll = event.srcElement.children[0].scrollTop; // 
+
+  //   //if not fixed
+  //   //and we have not yet scrolled until the original position of the element
+  //   //add the fixed class
+  //   if(this.fixedState == StickyState.noFixed && currentScroll + this.fixedViewportOffset < this.initialOffsetFromTop){
+  //       this.fixedState = StickyState.fixed;
+  //       this.renderer.addClass(this.element.nativeElement, 'fixed');
+  //   }
+  //   //if fixed
+  //   else if(this.fixedState == StickyState.fixed){
+  //     let currentOffsetFromTop = currentScroll + this.element.nativeElement.getBoundingClientRect().top;
+  //     //and the current offset from top is greater or equal than the original
+  //    //remove the fixed class
+  //     if (currentOffsetFromTop >= this.initialOffsetFromTop){
+  //       this.fixedState = StickyState.noFixed;
+  //       this.renderer.removeClass(this.element.nativeElement, 'fixed');
+  //     }
+  //   }
+  // }
 
 }
